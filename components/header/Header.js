97d05a3d6024f58a7a8397a221ef82fbc82e8f49
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-03-21 16:19:55 -0300

    Header created

--- components/header/Header.js
+++ components/header/Header.js
@@ -2,36 +2,74 @@ import useSWR from "swr";
 
 import ReactPlayer from "react-player";
 import { useState } from "react/cjs/react.development";
+import NavBar from "./nav-bar/NavBar";
+import { Button, Box, Container, Stack } from "@chakra-ui/react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const Header = () => {
   const { data, error } = useSWR(
-    "https://api.themoviedb.org/3/movie/297762/videos?api_key=86be5b5c8582aada04344dd370e7c0b2&append_to_response=videos",
+    "https://api.themoviedb.org/3/movie/76341/videos?api_key=86be5b5c8582aada04344dd370e7c0b2&append_to_response=videos",
     fetcher
   );
   const [muted, setMuted] = useState(true);
-
+  console.log(data?.results);
   return (
-    <div>
-      <h1>Wonder Woman</h1>
+    <Container
+      maxW="2400px"
+      h={["50vh", "50vh", "100vh", "100vh"]}
+      overflow="hidden"
+      p={0}
+    >
+      <NavBar />
 
-      <div>
+      <Box
+        h="100%"
+        w="100%"
+        maxW="2400px"
+        transform={["scale(3) ", "scale(2)", "scale(1.4)", "scale(1.4)"]}
+        zIndex={-1}
+      >
         <ReactPlayer
-          url={`https://www.youtube.com/watch?v=${data.results[3].key}`}
+          url={`https://www.youtube.com/watch?v=${data?.results[0].key}`}
           loop={true}
           playing={true}
           controls={false}
           muted={muted}
-          config={{
-            youtube: {
-              playerVars: false,
-            },
-          }}
+          width="100%"
+          height="100%"
         />
-      </div>
-      <button onClick={() => setMuted(!muted)}>mute</button>
-    </div>
+      </Box>
+      <Box
+        h="100%"
+        w="100%"
+        bg="transparent"
+        maxW="2400px"
+        pos="absolute"
+        bottom="0"
+        left="0"
+        zIndex={999}
+      >
+        <Stack w="100%" h="100%">
+          <Stack
+            h="100%"
+            justifyContent="flex-end"
+            alignItems="flex-end"
+            mr={5}
+          >
+            <Button
+              bg="transparent"
+              color="red.500"
+              onClick={() => setMuted(!muted)}
+              zIndex={100}
+              w="14"
+            >
+              mute
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
+    </Container>
   );
 };
 
